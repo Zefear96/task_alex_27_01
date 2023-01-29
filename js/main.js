5b87b7d0d4003d77ac1f50f0035e3a7fce1fdee1
Zamir <eazzy_freez@mail.ru>
2023-01-29 23:39:02 +0600

    fixed

--- js/main.js
+++ js/main.js
@@ -116,48 +116,54 @@ async function render() {
                             <p class="card-text"><b>Brand:</b> ${item.brand}</p>
                             <p class="card-text"><b>Description:</b> ${item.description}</p>
                             <p class="card-text"><b>Color:</b> ${item.color}</p>
-                            <p class="card-text"><b>Price:</b> ${item.price}$</p>
+                            <p class="card-text"><b>Price:</b> ${item.price} $</p>
                             <div class="d-flex flex-column justify-content-end">
                             <a href="#" class="btn btn-success mb-2 btn-add" id="add-${item.id}">Add to shopping cart</a>
-                            <a href="#" class="btn btn-danger btn-del" id="del-${item.id}">Delete shopping cart</a>
+                            <a href="#" class="btn btn-info mb-2 btn-upd" id="upd-${item.id}" 
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"
+                            >Update</a>
                             </div>
                      </div>
             </div>`
        });
 
-       
        addToCartShopEvent();
-       deleteToCartShopEvent()
-
+       
        // cart shop **********************************************************************************
        let shopCart = document.querySelector('.cart');
        shopCart.innerHTML = '';
        let productsCartShop = getProductsFromStorage();
+       // console.log(productsCartShop);
        productsCartShop.forEach(item => {
               shopCart.innerHTML += `<div class="card m-5 w-75 d-flex" style="width: 18rem;">
-              
                      <div class="card-body d-flex">
-                            <img src="${item.image}" class="img-fluid img-thumbnail" alt="error:(" height="50">
+                            <img src="${item.image}" class="img-fluid img-thumbnail w-25 h-25" alt="error:(">
                             <div class="container">
-                                   <h5 class="card-title">${item.name}</h5><hr>
+                                   <h5 class="card-title">${item.name}</h5>
+                                   <div class="d-flex justify-content-end">
+                                   <a href="#" class="btn btn-danger btn-del" id="del-${item.id}">Delete </a>
+                                   </div>
+                                   <hr>
                                    <p class="card-text">${item.brand}</p>
-                                   <p class="card-text">${item.color}</p>
-                                   <p class="card-text">Price: ${item.price}$</p>
-                                   <p class="card-text">Count: ${item.count}
-                                   <p class="card-text">Subprice: ${item.price*item.count}$</p>
+                                   <p class="card-text">color: ${item.color}</p>
+                                   <p class="card-text">price: ${item.price} $</p>
+                                   <p class="card-text">count: ${item.count}
+                                   <p class="card-text">subprice: ${item.price*item.count} $</p>
                                    <a href="#" class="btn btn-outline-success btn-add-count" id="del-${item.id}">+</a>
                                    <a href="#" class="btn btn-outline-danger btn-del-count" id="del-${item.id}">-</a>
-                                   </p>
                                    
                             </div>
-                            
                      </div>
             </div>`
        });
+
+       updateProdEvent();
+       deleteToCartShopEvent();
        addCountProdEvent();
        delCountProdEvent();
-       totalPrice()
-       
+       totalPrice();
+       if (productsCartShop.length == 0) return;
 
 };
 render()
@@ -174,8 +180,8 @@ async function setToLocalStorage(products) {
 };
 
 function getProductsFromStorage() {
-       let users = JSON.parse(localStorage.getItem('products-data'));
-       return users;
+       let products = JSON.parse(localStorage.getItem('products-data'));
+       return products;
 };
 
 async function addProdToCart(e) {
@@ -188,10 +194,10 @@ async function addProdToCart(e) {
        // console.log(prodObj);
        prodObj.count = 1;
        let product = getProductsFromStorage();
-       product.push(prodObj)
+       product.push(prodObj);
        setToLocalStorage(product)
        render();
-}
+};
 
 function addToCartShopEvent() {
        let btnAddToLocalStorage = document.querySelectorAll('.btn-add');
@@ -204,7 +210,7 @@ function deleteProdToCart(e) {
        let products = getProductsFromStorage();
        // console.log(products);
        products = products.filter(item => item.id != productId)
-       setToLocalStorage(products)
+       setToLocalStorage(products);
        render();
 };
 
@@ -239,7 +245,7 @@ function delCountProd(e) {
        // console.log(productObj);
 
        if (productObj.count === 1) {
-              return
+              return deleteProdToCart(e);
        } else productObj.count -= 1
        setToLocalStorage(products);
        render();
@@ -258,60 +264,94 @@ function totalPrice() {
        products.forEach(item => {
               return total += item.price * item.count;
        });
-       totalPrice.innerHTML = `Total price: ${total}$`;
+       totalPrice.innerHTML = `Total price: ${total} $`;
 };
 
 function clearLocalStorage() {
        localStorage.clear();
+       initStorage();
+       render()
+};
+
+let clearBtnLocal = document.querySelector('#btn-clear-local-storage');
+// console.log(clearBtnLocal);
+clearBtnLocal.addEventListener('click', clearLocalStorage);
+
+// group inputs
+let inpNameProd = document.querySelector('#inp-prod-name');
+let inpBrandProd = document.querySelector('#inp-prod-brand');
+let inpPriceProd = document.querySelector('#inp-prod-price');
+let inpDescProd = document.querySelector('#inp-prod-desc');
+let inpImageProd = document.querySelector('#inp-prod-image');
+let inpColorProd = document.querySelector('#inp-prod-color');
+// console.log(inpNameProd, inpBrandProd, inpPriceProd, inpDescProd, inpImageProd, inpColorProd);
+
+async function updateProd(e) {
+
+       let productId = e.target.id.split('-')[1];
+       // console.log(productId);
+
+       let res = await fetch(PRODUCTS_API);
+       let products = await res.json();
+
+       let productObj = products.find(item => item.id == productId);
+       // console.log(productObj);
+
+       inpNameProd.value = productObj.name;
+       inpBrandProd.value = productObj.brand;
+       inpPriceProd.value = productObj.price;
+       inpDescProd.value = productObj.description;
+       inpImageProd.value = productObj.image;
+       inpColorProd.value = productObj.color;
+
+       let btnSave = document.querySelector('.save-changes');
+       // console.log(btnSave);
+       btnSave.setAttribute('id', productId);
+       btnSave.addEventListener('click', saveChanges)
+};
+
+function updateProdEvent() {
+       let btnUpdProd = document.querySelectorAll('.btn-upd');
+       btnUpdProd.forEach(item => item.addEventListener('click', updateProd));
+};
+
+async function saveChanges(e) {
+       let productId = e.target.id
+       // console.log(productId);
+
+       let res = await fetch(PRODUCTS_API);
+       let products = await res.json();
+
+       let productObj = products.find(item => item.id == productId);
+       // console.log(productObj);
+
+       productObj.name = inpNameProd.value;
+       productObj.brand = inpBrandProd.value;
+       productObj.price = inpPriceProd.value;
+       productObj.description = inpDescProd.value;
+       productObj.image = inpImageProd.value;
+       productObj.color = inpColorProd.value;
+
+       fetch(`${PRODUCTS_API}/${productId}`, {
+              method: 'PATCH',
+              body: JSON.stringify(productObj),
+              headers: {
+                     'Content-Type': 'application/json;charset=utf-8'
+              }
+       })
+
+       inpNameProd.value = '';
+       inpBrandProd.value = '';
+       inpPriceProd.value = '';
+       inpDescProd.value = '';
+       inpImageProd.value = '';
+       inpColorProd.value = '';
+
+       let btnCloseChanges = document.querySelector('.btn-close');
+       btnCloseChanges.click();
+
+       e.target.removeAttribute('id');
        render();
 };
 
-let clearBtnLS = document.querySelector('#btn-clear-local-storage');
-clearBtnLS.addEventListener('click', clearLocalStorage);
-
-// Super Task
-// Реализовать имитацию блога
-// 1. Каждый пользователь должен иметь возможность зарегистрироваться и авторизоваться на сайте, вид объекта пользователя:
-// {
-//   id: 12,
-//   name: 'Jack',
-//   password: 'superJack',
-//   favorites: []
-// }
-
-// 2. Полный CRUD на посты, каждый зарегистрированный и авторизованный пользователь должен получить доступ к созданию, редактированию, удалению ТОЛЬКО СВОИХ постов, вид объекта поста:
-// {
-//   id: 4,
-//   title: SomeTitle,
-//   content: SomeContent,
-//   likes: 8,
-//   author: {
-//     id: 12,
-//     name: 'Jack',
-//   }
-// }
-
-// 3. Незарегистрированные и неавторизованные пользователи могут только просматривать посты
-
-// 4. Каждый пользователь может поставить лайк любому посту, после чего пост, который нравится пользователю, отображается в массиве favorites, например пользователю с id-12, нравится пост с id-4, пользователь ставит лайк посту, пост добавляется в массив favorites, который находится в объекте пользователя:
-// {
-//   id: 12,
-//   name: 'Jack',
-//   password: 'superJack',
-//   favorites: [
-//     {
-//       id: 4,
-//       title: SomeTitle,
-//       content: SomeContent,
-//       likes: 8
-//     }
-//   ]
-// }, в свою очередь количество лайков у поста повышается:
-// {
-//   id: 4,
-//   title: SomeTitle,
-//   content: SomeContent,
-//   likes: 8 -> 9
-// }, также у поста кнопка ЛАЙК должна измениться на ДИЗЛАЙК, при нажатии количество лайков у поста должно уменьшиться на 1, а у пользователя данный пост должен быть удален из массива favorites
-
-// ВАЖНО: если пользователь ставик лайк посту, то для него исчезает кнопка лайк и появляется кнопка дизлайк(для данного поста), но если зайти под другим аккаунтом, который еще не ставил лайк, то кнопка лайк снова должна появиться 
+       
